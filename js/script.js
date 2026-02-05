268ed4b0dfef1a491e61eadc6998b71ee218fc72
Botagoz Aimkulova <aimkulow@bk.ru>
2026-02-05 13:35:15 +0500

    add category for catalog

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-  // Меню связаться
-  document.querySelector(".floating-btn").addEventListener("click", function () {
-    const menu = document.querySelector(".floating-menu");
-    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
-  });
-
-  //   Проверка поля номер телефона в заявке
-  const phoneInput = document.querySelector('input[name="phone"]');
-  phoneInput.addEventListener("input", function (e) {
-    let x = e.target.value.replace(/\D/g, "").match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
-    e.target.value =
-      "+7 " + (x[2] ? x[2] : "") + " " + (x[3] ? x[3] : "") + " " + (x[4] ? x[4] : "") + " " + (x[5] ? x[5] : "");
-  });
-
   //Бургер меню
   const burger = document.getElementById("burgerBtn");
   const menu = document.getElementById("mobileMenu");
@@ -28,5 +14,19 @@ $(document).ready(function () {
       burger.classList.remove("active");
       menu.classList.remove("active");
     }
+  });
+
+  // Меню связаться
+  document.querySelector(".floating-btn").addEventListener("click", function () {
+    const menu = document.querySelector(".floating-menu");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+  });
+
+  //   Проверка поля номер телефона в заявке
+  const phoneInput = document.querySelector('input[name="phone"]');
+  phoneInput.addEventListener("input", function (e) {
+    let x = e.target.value.replace(/\D/g, "").match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    e.target.value =
+      "+7 " + (x[2] ? x[2] : "") + " " + (x[3] ? x[3] : "") + " " + (x[4] ? x[4] : "") + " " + (x[5] ? x[5] : "");
   });
 });
